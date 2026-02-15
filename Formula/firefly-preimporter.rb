b316b94174808a23ffa5192355a5a55efa597fbf
Humberto Gonzalez <1747263+hgonzale@users.noreply.github.com>
2026-02-15 01:28:29 -0800

    Add firefly-preimporter formula v0.4.1

---
+++ Formula/firefly-preimporter.rb
@@ -0,0 +1,50 @@
+class FireflyPreimporter < Formula
+  include Language::Python::Virtualenv
+
+  desc "Transaction statement preprocessor for Firefly III / FiDI"
+  url "https://github.com/hgonzale/firefly-preimporter/releases/download/v0.4.1/firefly_preimporter-0.4.1.tar.gz"
+  sha256 "71d33ed96be961f2891551202688add93d28cdb6afa78e7217c4274a3af56a4f"
+  version "0.4.1"
+
+  depends_on "python@3.11"
+
+  # --- begin generated resources ---
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/a2/8c/58f469717fa48465e4a50c014a0400602d3c437d7c0c468e17ada824da3a/certifi-2025.11.12.tar.gz"
+    sha256 "d8ab5478f2ecd78af242878415affce761ca6bc54a22a27e026d7c25357c3316"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/13/69/33ddede1939fdd074bce5434295f38fae7136463422fe4fd3e0e89b98062/charset_normalizer-3.4.4.tar.gz"
+    sha256 "94537985111c35f28720e43603b8e7b43a6ecfb2ce1d3058bbe955b73404e21a"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/6f/6d/0703ccc57f3a7233505399edb88de3cbd678da106337b9fcde432b65ed60/idna-3.11.tar.gz"
+    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
+  end
+
+  resource "ofxtools" do
+    url "https://files.pythonhosted.org/packages/ce/37/d224428cbb44b8d36b9458c2c9b11eb54ece95bc455ef6f7ada583809b13/ofxtools-0.9.5.tar.gz"
+    sha256 "682a516bfa5ccad0f9551c17cc2cf155422f9f5f85a341cfb4911b324de46045"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4/requests-2.32.5.tar.gz"
+    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/1c/43/554c2569b62f49350597348fc3ac70f786e3c32e7f19d266e19817812dd3/urllib3-2.6.0.tar.gz"
+    sha256 "cb9bcef5a4b345d5da5d145dc3e30834f58e8018828cbc724d30b4cb7d4d49f1"
+  end
+  # --- end generated resources ---
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/firefly-preimporter --version")
+  end
+end
